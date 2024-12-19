3847dd64ef4f5571d7b5e912fab8e385dc39e03b
kunillet <herman.daniel.berrio@estudiantat.upc.edu>
2024-12-19 07:38:55 +0100

    simplificado footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,9 @@ function Footer() {
     <div style={{ textAlign: "center", marginTop: "20px" }}>
       <hr style={{ border: "0", height: "1px", backgroundColor: "#ff6600", margin: "10px 0" }} />
       <span className="yclinks" style={{ fontSize: "8pt", color: "#828282" }}>
-        <Link to="/newsguidelines">Guidelines</Link> |{" "}
-        <Link to="/newsfaq">FAQ</Link> |{" "}
-        <Link to="/lists">Lists</Link> |{" "}
-        <a href="https://github.com/HackerNews/API" target="_blank" rel="noopener noreferrer">API</a> |{" "}
-        <Link to="/security">Security</Link> |{" "}
-        <Link to="/legal">Legal</Link> |{" "}
-        <Link to="/apply">Apply to YC</Link> |{" "}
-        <Link to="/contact">Contact</Link>
+        | {" "}
+          <Link to="/contact">Contact</Link>
+        {" "} |
       </span>
       <div style={{ marginBottom: "20px" }}></div>
     </div>
